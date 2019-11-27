69090ee6103c115d1ac00bffa68bedeff5dcec2c
Jamie Carter <jc2820@hotmail.com>
2019-11-27 15:27:00 +0000

    Add file structure and basic html

---
+++ public/main.js
@@ -0,0 +1,1 @@
+const placeholder = 'placeholder';
--- test.js
+++
@@ -1,8 +0,0 @@
-    const func = () => {
-    var variable = "f";
-    let tim = 2;
-    console.log(tim);
-    console.log(variable);
-    }
-
-    func();
