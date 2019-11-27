73d8a79cb80a4697d4252ae7c5373a15a9f8b1b6
Pat Cichocki <pat.cichocki.uk@gmail.com>
2019-11-27 20:41:52 +0000

    createUrl function tests

--- public/main.js
+++ public/main.js
@@ -1,1 +1,24 @@
-const placeholder = 'placeholder';
+// const quantityInput = document.getElementById('quantity');
+// let cryptoCurrency = document.getElementById('first-currency');
+// // const submitButton = document.getElementById('submit-button');
+// let cashCurrency = document.getElementById('second-currency');
+
+
+// let crypto = cryptoCurrency.value;
+// let usd = cashCurrency.value;
+
+// let url = '';
+
+const URL1 = "https://api.coingecko.com/api/v3/simple/price?ids=";
+const URL2 = "&vs_currencies=";
+
+const createUrl = (firstCurrency, secondCurrency) => {
+  return `${URL1}${firstCurrency}${URL2}${secondCurrency}` ;
+};
+
+
+
+// createUrl(crypto, usd);
+
+module.exports = {createUrl};
+  
--- tests/test.js
+++ tests/test.js
@@ -1,1 +1,28 @@
 const test = require('tape');
+const main = require('../public/main');
+
+//createUrl output tests:
+
+test("createUrl returns a string", t => {
+    const actual = typeof main.createUrl("bitcoin", "usd");
+    t.equal(actual, "string");
+    t.end();
+});
+
+test("createUrl returns a string that contains first passed currency", t => {
+    const actual =  main.createUrl("bitcoin", "usd").includes("bitcoin");
+    t.equal(actual, true);
+    t.end();
+});
+
+test("createUrl returns a string that contains second passed currency", t => {
+    const actual =  main.createUrl("bitcoin", "usd").includes("usd");
+    t.equal(actual, true);
+    t.end();
+});
+
+test("createUrl returns a string that contains 'https' substring", t => {
+    const actual =  main.createUrl("bitcoin", "usd").includes("https");
+    t.equal(actual, true);
+    t.end();
+});
