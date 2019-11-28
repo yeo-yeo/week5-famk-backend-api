e680df1bbbe1a272ac50fda5cc6d541735491bb3
Pat Cichocki <pat.cichocki.uk@gmail.com>
2019-11-28 11:40:33 +0000

    inital dom

--- public/main.js
+++ public/main.js
@@ -1,6 +1,18 @@
-// const quantityInput = document.getElementById('quantity');
-// let cryptoCurrency = document.getElementById('first-currency');
-// // const submitButton = document.getElementById('submit-button');
-// let cashCurrency = document.getElementById('second-currency');
+const quantityInput = document.querySelector('.quantity');
+const 
+ = document.getElementById('first-currency');
+const submitButton = document.getElementById('submit-button');
+const cashCurrency = document.getElementById('second-currency');
 
+let queryObj = {};
 
+submitButton.addEventListsener('click', e => {
+    
+    e.preventDefault()
+    const quantity = quantityInput.value;
+    const cryptoCurrency = currencyInput.value;
+    queryObj.quantity = quantity;
+    queryObj.cryptoCurrency = cryptoCurrency;
+    console.log(queryObj);
+
+})
