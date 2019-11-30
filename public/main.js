d5e7e6aa7ed0ab988096c652e0055a6a916f4a97
Gillian <gillian@yeomans.org.uk>
2019-11-30 13:08:10 +0000

    change dropdown element and add front-end validation for quantity

--- public/main.js
+++ public/main.js
@@ -1,73 +1,82 @@
-//UI Variables
+//DOM Variables
 const quantityInput = document.querySelector(".quantity");
-const currencyInput = document.querySelector("#first-currency");
-const normalCurrencyInput = document.querySelector("#second-currency");
+const cryptoCurrencyInput = document.querySelector("#first-currency");
+const targetCurrencyInput = document.querySelector("#second-currency");
 const submitButton = document.querySelector("#submit-button");
 const resultContainer = document.querySelector("#result");
-let quantity;
-let cryptoCurrency;
-let normalCurrency;
+
+//values
+let apiResponse;
 
 submitButton.addEventListener("click", event => {
-  quantity = quantityInput.value;
-  cryptoCurrency = currencyInput.value;
-  normalCurrency = normalCurrencyInput.value;
   event.preventDefault();
-  console.log("button event list");
-  backEndCall(cryptoCurrency, normalCurrency);
+  let quantity = quantityInput.value;
+  //validation to check quantity is a number (nb is read as a string then converted)
+  if (Number(quantity)) {
+    let cryptoCurrency = cryptoCurrencyInput.value;
+    let targetCurrency = targetCurrencyInput.value;
+    backEndCall(cryptoCurrency, targetCurrency, quantity);
+  } else {
+    alert("Please enter a valid quantity number");
+  }
 });
 
-// let APIResponse;
-
-const backEndCall = (cryptoC, normalC) => {
+const backEndCall = (cryptoC, targetC, quantity) => {
   const xml = new XMLHttpRequest();
-  const url = `/search?ids=${cryptoC}&vs_currencies=${normalC}`;
+  const url = `/search?ids=${cryptoC}&vs_currencies=${targetC}`;
   xml.onreadystatechange = () => {
     if (xml.readyState === 4 && xml.status === 200) {
       apiResponse = JSON.parse(xml.responseText);
-      console.log("this is api response", apiResponse);
-      renderCurrencies();
+      renderCurrencies(cryptoC, targetC, quantity);
     }
   };
   xml.open("GET", url, true);
   xml.send();
 };
 
+const renderCurrencies = (cryptoC, targetC, quantity) => {
+  //clear any existing result
+  clearElement(resultContainer);
+
+  //multiply currency value by user inputted quantity and format
+  let totalValue = (
+    apiResponse[cryptoC.toLowerCase()][targetC.toLowerCase()] * quantity
+  )
+    .toFixed(2)
+    .replace(/\d(?=(\d{3})+\.)/g, "$&,");
+  const currSymbol = chooseCurrencySymbol(targetC);
+
+  //add dom element
+  let displayValue = document.createElement("p");
+  displayValue.classList.add("display_value");
+  displayValue.textContent = `${currSymbol} ${totalValue}`;
+  resultContainer.appendChild(displayValue);
+
+  //add last updated at timestamp dom element
+  let timeStamp = createTimeStamp(
+    apiResponse[cryptoC.toLowerCase()].last_updated_at
+  ).toString();
+  let uiTimeStamp = document.createElement("p");
+  uiTimeStamp.textContent = `Last updated: ${timeStamp.slice(0, 34)}`;
+  uiTimeStamp.classList.add("time_stamp");
+  resultContainer.appendChild(uiTimeStamp);
+};
+
+//to clear an existing result when the search is rerun
 const clearElement = el => {
   while (el.firstChild) {
     el.removeChild(el.firstChild);
   }
 };
 
-const renderCurrencies = () => {
-  clearElement(resultContainer);
-  let displayValue = document.createElement("p");
-  let totalValue = (
-    apiResponse[cryptoCurrency.toLowerCase()][normalCurrency.toLowerCase()] *
-    quantity
-  )
-    .toFixed(2)
-    .replace(/\d(?=(\d{3})+\.)/g, "$&,");
-  const currSymbol = chooseCurrencySymbol(normalCurrency);
-  displayValue.textContent = `${currSymbol} ${totalValue}`;
-  displayValue.classList.add("display_value");
-  resultContainer.appendChild(displayValue);
-  let ccTimeStamp = createTimeStamp(
-    apiResponse[cryptoCurrency.toLowerCase()].last_updated_at
-  );
-  let stringDate = ccTimeStamp.toString();
-  let uiTimeStamp = document.createElement("p");
-  uiTimeStamp.textContent = `Last updated: ${stringDate.slice(0, 34)}`;
-  uiTimeStamp.classList.add("time_stamp");
-  resultContainer.appendChild(uiTimeStamp);
-};
-
+//to format the time from seconds into a date
 const createTimeStamp = secs => {
   var t = new Date(1970, 0, 1);
   t.setSeconds(secs);
   return t;
 };
 
+//for displaying the result
 const chooseCurrencySymbol = curr => {
   const lookup = {
     USD: "$",
